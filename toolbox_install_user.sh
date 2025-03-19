5dc02546b2c72f4d37c463696ff95f8e7f859317
Ales Nosek <ales.nosek@gmail.com>
2025-03-18 20:23:43 -0700

    SKip compaudit in zsh

--- toolbox_install_user.sh
+++ toolbox_install_user.sh
@@ -10,6 +10,10 @@ rcfiles_dir=~/git/github.com/noseka1/rcfiles
 git clone https://github.com/noseka1/rcfiles $rcfiles_dir
 $rcfiles_dir/bin/rcfiles_setup --assumeyes
 
+# Skip compaudit. It doesn't like completion files owned by a different user.
+# See also https://stackoverflow.com/questions/13762280/zsh-compinit-insecure-directories
+sed -i 's/^compinit$/compinit -u/' ~toolbox/.zshrc.in
+
 # Replace absolute symlinks with relative ones so that the toolbox user's
 # configuration files can be copied to another user's home directory
 symlinks -rc ~
