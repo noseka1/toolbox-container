2d1106adaa9bf2d70392675df332bde7701c6d89
Ales Nosek <ales.nosek@gmail.com>
2025-03-23 12:55:49 -0700

    Set /bin/bash if /bin/zsh is not available

--- toolbox_fix_user.sh
+++ toolbox_fix_user.sh
@@ -6,7 +6,8 @@ set -euo pipefail
 # This script can be invoked from inside a running container in OpenShift.
 
 if ! whoami &> /dev/null; then
+  [ -x /bin/zsh ] && shell=/bin/zsh || shell=/bin/sh
   # Can't use sed -i due to permissions on /etc
-  tmppasswd=$(sed "s#toolbox:.*#toolbox:x:$(id -u):0:Toolbox user:/home/toolbox:/bin/zsh#" /etc/passwd)
+  tmppasswd=$(sed "s#toolbox:.*#toolbox:x:$(id -u):0:Toolbox user:/home/toolbox:$shell#" /etc/passwd)
   echo "$tmppasswd" > /etc/passwd
 fi
