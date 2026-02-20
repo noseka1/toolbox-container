4888a0c3421b1fd1fa8386f47d92c7fc61d3fe5d
Ales Nosek <ales.nosek@gmail.com>
2026-02-20 10:14:21 -0800

    Use single configmap toolbox-container-scripts

--- deploy/run.sh
+++ deploy/run.sh
@@ -2,4 +2,12 @@
 
 echo Hello from the custom run script!
 
-gunicorn-3 --bind 0.0.0.0:8080 --access-logfile - httpbin:app
+#python3 -m http.server 8080
+
+#gunicorn-3 --bind 0.0.0.0:8080 --access-logfile - httpbin:app
+
+echo
+echo Press Ctrl-C to exit ...
+
+trap : TERM INT
+sleep infinity & wait
